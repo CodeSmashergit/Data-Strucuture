92c28cf71e7ced9660bf5bab54ca433f429f0fd7
codesmashergit <shubham69385@gmail.com>
2024-11-06 12:48:09 +0530

    Added msg to Test_branch_1 second commit

--- DataStructure/src/DS/Testing1.java
+++ DataStructure/src/DS/Testing1.java
@@ -4,9 +4,15 @@ public class Testing1 {
     public static void main(String[] args) {
         System.out.println("Testing1");
         System.out.println("Next");
+        displayMsg();
+        printMsg();
     }
 
     public static void printMsg() {
         System.out.println("Has to be removed from Test_branch");
+    }
+
+    public static void displayMsg() {
+        System.out.println("Display Test_Branch to be removed");
     }
 }
